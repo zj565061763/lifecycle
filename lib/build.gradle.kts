5681b0273c52684770ba8cbad1971f2b5ede0796
zjun <565061763@qq.com>
2024-12-26 00:52:53 +0800

    1.1.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "lifecycle"
-val libVersion = "1.0.0"
+val libVersion = "1.1.0"
 
 android {
   namespace = "com.sd.lib.lifecycle"
